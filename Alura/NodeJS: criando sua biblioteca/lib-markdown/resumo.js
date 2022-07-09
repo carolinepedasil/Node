7c9d07162351b9da4f177aab2b6b4b8f9be53076
Caroline P. da Silva <carolinepedasil@gmail.com>
2022-07-08 23:45:03 -0300

    Node

--- Alura/NodeJS: criando sua biblioteca/lib-markdown/resumo.js
+++ Alura/NodeJS: criando sua biblioteca/lib-markdown/resumo.js
@@ -36,10 +36,38 @@ function texto(string) {
 
 console.log(texto(paragrafo));
 
+// Promises
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => chalk.green(console.log(texto)))
+    .catch((erro) => trataErro(erro))
+}
+
+// Outra Forma:
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if (erro) {
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto));
+    })
+}
+
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    // const linksExtraidos = texto.match(regex); // match -> string
+    const linksExtraidos = regex.exec(texto); // exec -> regex
+    console.log(linksExtraidos);
+}
+
 /*
     - As dependências (também chamadas de libs ou bibliotecas) são pacotes de código pronto que outras pessoas da comunidade disponibilizam, e nós as utilizamos para executar tarefas específicas no código sem termos que reescrever do zero;
     - Para deixar o código mais organizado podemos usar ferramentas de linting, além da importância delas para reforçar estilo e também para prever possíveis bugs.
     - Podemos utilizar a lib fs (File System, ou sistema de arquivos) nativa do JavaScript para que o programa consiga acessar e ler arquivos do computador;
     - Conseguimos capturar mensagens de erro enviadas pelo NodeJS quando algo no programa não sai como o esperado utilizando a palavra-chave throw, ou lançar;
     - As “promessas” são a forma que o JavaScript utiliza para trabalhar com código assíncrono e que podemos resolvê-las utilizando em conjunto as palavras-chave async e await ou o método .then().
+    - Como utilizamos classes e grupos de expressões regulares para refinar a busca por estes padrões, além dos métodos que o JavaScript tem (por exemplo match e exec) para trazer os resultados e trabalhar com eles;
 */
