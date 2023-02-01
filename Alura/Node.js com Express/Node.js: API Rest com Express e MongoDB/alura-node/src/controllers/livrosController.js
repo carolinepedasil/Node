74ee29432ecbee71c829453f9f4e6a8b787e5b74
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-01-31 23:16:42 -0300

    Aprimorando o projeto

--- Alura/Node.js com Express/Node.js: API Rest com Express e MongoDB/alura-node/src/controllers/livrosController.js
+++ Alura/Node.js com Express/Node.js: API Rest com Express e MongoDB/alura-node/src/controllers/livrosController.js
@@ -2,7 +2,9 @@ import livros from "../models/Livro.js";
 
 class LivroController {
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
+        livros.find()
+            .populate('autor')
+            .exec((err, livros) => {
             res.status(200).json(livros);
         });
     }
@@ -10,7 +12,9 @@ class LivroController {
     static listarLivroPorId = (req, res) => {
         const id = req.params.id;
 
-        livros.findById(id, (err, livros) => {
+        livros.findById(id)
+            .populate('autor', 'nome')
+            .exec((err, livros) => {
             if(err) {
                 res.status(400).send({message: `${err.message} - Id do livro não localizado.`});
             } else {
@@ -53,6 +57,14 @@ class LivroController {
                 res.status(500).send({message: err.message});
             }
         })
+    }
+
+    static listarLivroPorEditora = (req, res) => {
+        const editora = req.query.editora;
+
+        livros.find({'editora': editora}, {}, (err, livros) => {
+            res.status(200).send(livros);
+        });
     }
 }
 
