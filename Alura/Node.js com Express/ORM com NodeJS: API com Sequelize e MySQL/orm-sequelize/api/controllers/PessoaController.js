bb64ef40a10f1b23bc3477889bb8bdd4f55b3c86
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-02-03 23:14:08 -0300

    Controladores e rotas

---
+++ Alura/Node.js com Express/ORM com NodeJS: API com Sequelize e MySQL/orm-sequelize/api/controllers/PessoaController.js
@@ -0,0 +1,23 @@
+const database = require('../models')
+
+class PessoaController {
+    static async pegaTodasAsPessoas(req, res){
+        try {
+            const todasAsPessoas = await database.Pessoas.findAll()
+            return res.status(200).json(todasAsPessoas)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }      
+    }
+
+    static async pegaTodasAsPessoas(req, res){
+        try {
+            const todasAsPessoas = await database.Pessoas.findAll()
+            return res.status(200).json(todasAsPessoas)  
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+}
+
+module.exports = PessoaController
