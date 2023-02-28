49f5a789860d44d61bb4c203539c7aff6ada93dd
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-02-28 20:54:39 -0300

    Organizando o projeto e injeção de dependências

--- Alura/Nest.js/APIs REST com NestJS: buscas, validação, serialização e detalhes arquiteturais/alurapic-server/src/usuario/usuario.service.ts
+++ Alura/Nest.js/APIs REST com NestJS: buscas, validação, serialização e detalhes arquiteturais/alurapic-server/src/usuario/usuario.service.ts
@@ -1,10 +1,25 @@
+import { Injectable } from "@nestjs/common";
+import { Usuario } from "./usuario.entity";
+
+@Injectable()
 export class UsuarioService {
 
-    private usuarios = [];
+    private usuarios: Array<Usuario> = [{
+        id: 1,
+        nomeDeUsuario: 'caroline',
+        email: 'caroline.pereira@email.com',
+        senha: '123456',
+        nomeCompleto: 'Caroline Pereira',
+        dataDeEntrada: new Date()
+    }];
 
-    public cria(usuario){
+    public cria(usuario: Usuario): Usuario {
         this.usuarios.push(usuario);
         return usuario;
+    }
+
+    public buscaPorNomeDeUsuario(nomeDeUsuario: string): Usuario {
+        return this.usuarios.find(usuario => usuario.nomeDeUsuario == nomeDeUsuario);
     }
 
 }
