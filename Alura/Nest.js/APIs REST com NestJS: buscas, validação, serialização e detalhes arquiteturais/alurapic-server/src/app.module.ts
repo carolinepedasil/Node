40262d7636acaf80c14fca84cb08a58319c1078f
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-03-02 23:07:40 -0300

    Serialização e tratamento de erros

--- Alura/Nest.js/APIs REST com NestJS: buscas, validação, serialização e detalhes arquiteturais/alurapic-server/src/app.module.ts
+++ Alura/Nest.js/APIs REST com NestJS: buscas, validação, serialização e detalhes arquiteturais/alurapic-server/src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.modules';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { filtroDeExcecaoHttp } from './common/filtros/filtro-de-excecao-http.filter';
 
 @Module({
   imports: [UsuarioModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: filtroDeExcecaoHttp
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor
+    }
+  ],
 })
 export class AppModule {}
--- Alura/Nest.js/APIs REST com NestJS: buscas, validação, serialização e detalhes arquiteturais/alurapic-server/src/usuario/usuario.entity.ts
+++ Alura/Nest.js/APIs REST com NestJS: buscas, validação, serialização e detalhes arquiteturais/alurapic-server/src/usuario/usuario.entity.ts
@@ -1,23 +1,30 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 import { IsNomeDeUsuarioUnico } from "./is-nome-de-usuario-unico.validator";
+import { Exclude, Expose } from "class-transformer";
 
 export class Usuario {
 
     id: number;
 
+    @Expose({ name: 'username' })
     @IsNomeDeUsuarioUnico({ message: 'Nome de Usuário precisa ser único!' })
     @IsNotEmpty({ message: 'Nome de Usuário é obrigatório!' })
     @IsString({ message: 'Nome de Usuário precisa ser uma string!' })
     nomeDeUsuario: string;
 
+    @Expose({ name: 'email' })
     @IsEmail({}, { message: 'E-mail precisa ser um endereço de e-mail válido!' })
     email: string;
 
+    @Expose({ name: 'password' })
+    @Exclude({ toPlainOnly: true })
     @IsNotEmpty({ message: 'Senha é obrigatória!' })
     senha: string;
 
+    @Expose({ name: 'fullName' })
     @IsNotEmpty({ message: 'Nome Completo é obrigatório!' })
     nomeCompleto: string;
 
+    @Expose({ name: 'joinDate' })
     dataDeEntrada: Date;
 }
