63870d5a36903cf0d626063a65bfed4286e538cd
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-03-03 23:49:03 -0300

    Aplicando padrões do REST

--- Alura/Nest.js/APIs REST com NestJS: buscas, validação, serialização e detalhes arquiteturais/alurapic-server/src/usuario/usuario.controller.ts
+++ Alura/Nest.js/APIs REST com NestJS: buscas, validação, serialização e detalhes arquiteturais/alurapic-server/src/usuario/usuario.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpStatus, NotFoundException, Param, Post } from "@nestjs/common";
 import { UsuarioService } from "./usuario.service";
 import { Usuario } from "./usuario.entity";
+import { NestResponse } from "../core/http/nest-response";
+import { NestResponseBuilder } from "../core/http/nest-response-builder";
 
 @Controller('users')
 export class UsuarioController {
@@ -10,13 +12,25 @@ export class UsuarioController {
     @Get(':nomeDeUsuario')
     public buscaPorNomeDeUsuario(@Param('nomeDeUsuario') nomeDeUsuario: string){
         const usuarioEncontrado = this.usuarioService.buscaPorNomeDeUsuario(nomeDeUsuario);
+        if (!usuarioEncontrado) {
+            throw new NotFoundException({
+                statusCode: HttpStatus.NOT_FOUND,
+                message: 'Usuário não encontrado!'
+            });
+        }
         return usuarioEncontrado;
     }
 
     @Post()
-    public cria(@Body() usuario: Usuario): Usuario {
+    public cria(@Body() usuario: Usuario): NestResponse {
         const usuarioCriado = this.usuarioService.cria(usuario);
-        return usuarioCriado;
+        return new NestResponseBuilder()
+            .comStatus(HttpStatus.CREATED)
+            .comHeaders({
+                'Location': `/users/${usuarioCriado.nomeDeUsuario}`
+            })
+            .comBody(usuarioCriado)
+            .build();
     }
 
 }
