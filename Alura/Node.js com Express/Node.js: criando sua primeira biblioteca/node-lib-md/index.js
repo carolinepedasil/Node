700e271a56a590f507542ea13328597768e118f5
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-03-29 23:36:37 -0300

    Carregamento de arquivos

--- Alura/Node.js com Express/Node.js: criando sua primeira biblioteca/node-lib-md/index.js
+++ Alura/Node.js com Express/Node.js: criando sua primeira biblioteca/node-lib-md/index.js
@@ -1,5 +1,41 @@
+import fs, { fdatasyncSync } from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('olá mundo'));
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório!'));
+}
 
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).');
+// Assync/Await
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro);
+    } 
+}
+
+// Promises com then ()
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch(trataErro)
+// }
+
+// Função que não envolve promessas
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro) {
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     });
+// }
+
+pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
