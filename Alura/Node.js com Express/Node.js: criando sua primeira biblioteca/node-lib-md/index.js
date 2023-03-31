a57c7ec8262f6bca26a444bfa6ae9a73272164b5
Caroline P. da Silva <carolinepedasil@gmail.com>
2023-03-30 22:31:30 -0300

    Capturando links

--- Alura/Node.js com Express/Node.js: criando sua primeira biblioteca/node-lib-md/index.js
+++ Alura/Node.js com Express/Node.js: criando sua primeira biblioteca/node-lib-md/index.js
@@ -1,5 +1,12 @@
-import fs, { fdatasyncSync } from 'fs';
+import fs from 'fs';
 import chalk from 'chalk';
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados;
+}
 
 function trataErro(erro) {
     console.log(erro);
@@ -8,34 +15,13 @@ function trataErro(erro) {
 
 // Assync/Await
 async function pegaArquivo(caminhoDoArquivo) {
-    try {
+    try { 
         const encoding = 'utf-8';
-        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(chalk.green(texto));
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(extraiLinks(texto));
     } catch (erro) {
-        trataErro(erro);
-    } 
+        trataErro(erro)
+    }
 }
 
-// Promises com then ()
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//     fs.promises
-//         .readFile(caminhoDoArquivo, encoding)
-//         .then((texto) => console.log(chalk.green(texto)))
-//         .catch(trataErro)
-// }
-
-// Função que não envolve promessas
-// function pegaArquivo(caminhoDoArquivo) {
-//     const encoding = 'utf-8';
-//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//         if (erro) {
-//             trataErro(erro);
-//         }
-//         console.log(chalk.green(texto));
-//     });
-// }
-
 pegaArquivo('./arquivos/texto.md');
-pegaArquivo('./arquivos/');
